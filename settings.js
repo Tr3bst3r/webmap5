a5f43edabc2aaa9e20cf25002a72fab2444ef334
Tr3bst3r <sprechep@gmail.com>
2025-06-30 18:49:30 -0500

    new

---
+++ settings.js
@@ -0,0 +1,8 @@
+var settings = {
+    title: "My VS Server",
+    titleBarName: "My VS",
+    titleBarCommunity: "My Community",
+    siteUrl: "https://example.com",
+    updateText : "Last updated on ",
+    lastUpdated : "Monday, June 30, 2025."
+}
